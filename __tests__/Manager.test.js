b65dc019de272e127b5122fd3067120f35092fd6
Delgado <rxd718@miami.edu>
2023-02-20 09:22:41 -0500

    fixed up test code

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -3,14 +3,14 @@ const Manager = require('../lib/Manager');
 
 // creating manager object  
 test('creates an Manager object', () => {
-    const manager = new Manager('Nicole', 90, 'nicole.elisaw@gmail', 4);
+    const manager = new Manager('Maximus', 24, 'dasupermax@gmail.com');
     
     expect(manager.officeNumber).toEqual(expect.any(Number));
 });
 
 // gets role from getRole()
 test('gets role of employee', () => {
-    const manager = new Manager('Nicole', 90, 'nicole.elisaw@gmail.com');
+    const manager = new Manager('Maximus', 24, 'dasupermax@gmail.com');
 
     expect(manager.getRole()).toEqual("Manager");
 }); 
